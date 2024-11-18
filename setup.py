9a65ce8eaa798a2baac44377e53278d496fe26e9
Carter Sifferman <cpsiff@gmail.com>
2024-11-18 11:37:28 -0600

    include dependencies and fix setup install

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import find_packages, setup
 from glob import glob
+import os
 
 package_name = 'mini_tof'
+
+# Get the list of files in the readers subdirectory
+readers_files = glob(os.path.join(package_name, 'readers', '*'))
+readers_files = [f for f in readers_files if os.path.isfile(f)]  # Ensure only files are included
 
 setup(
     name=package_name,
@@ -12,8 +17,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         # copy everything in the "readers" directory to the install location, so that we can
-        # access them with , e.g. 'from readers import tmf8820_reader".
-        ('lib/' + package_name + '/readers', glob(package_name + '/readers/*')),
+        # access them with e.g., 'from readers import tmf8820_reader".
+        ('lib/' + package_name + '/readers', readers_files),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
