57d5456df660c0b3a206f52b36b5931a27a1135a
William Sun <wis003@ucsd.edu>
2025-03-23 11:01:46 -0500

    add aggregation code

--- mini_tof/readers/vl53l8ch_reader.py
+++ mini_tof/readers/vl53l8ch_reader.py
@@ -11,6 +11,7 @@ import serial
 from mini_tof_interfaces.msg import DepthEstimate, ToFFrame, ToFHistogram
 
 class VL53L8CHReader:
+
     def __init__(self, port, num_zones):
         self.mcu = serial.Serial(
             port,
@@ -33,23 +34,46 @@ class VL53L8CHReader:
     def get_measurement(self):
         self.mcu.reset_input_buffer()
 
-        frame_data = []
-        valid_frame = True
-        expected_zone_idx = 0
+        started = False
+        second_cycle = False
+        concat_frame_data = [None] * self.num_zones
+        concat_ambient_light = [None] * self.num_zones
+        i = 0
+        while i < 3: # 3 frames per shift
+            frame_data = {}
+        
+            # We expect zone idx 0 to come first - if not, bad frame and throw it out until we see
+            # zone idx 0
+            expected_zone_idx = 0
+            while expected_zone_idx < self.num_zones:
+                try:
+                    zone_idx, ambient_light, zone_data, _, distance_mm, range_sigma_mm, reading_counter, start_bin = (
+                        self.readline_and_decode(self.mcu)
+                    )
+                except Exception as e:
+                    print(f"(1) Notice, reading serial data: {e}")
+                    continue
+                if reading_counter == 0 and start_bin == 0 and started:
+                    second_cycle = True # after the second cycle, data is consistent
+                if reading_counter == 0 and start_bin == 0:
+                    started = True
 
-        # Read data for all zones in the frame.
-        while expected_zone_idx < self.num_zones:
-            zone_idx, zone_data, _ = VL53L8CHReader.readline_and_decode(self.mcu)
-            if zone_idx != expected_zone_idx:
-                # print(f"Warning: Expected zone {expected_zone_idx} but received zone {zone_idx}")
-                valid_frame = False
-                break
-
-            frame_data.append(zone_data)
-            expected_zone_idx += 1
-
-        if valid_frame:
-            return frame_data
+                frame_data[zone_idx] = zone_data
+                if zone_idx == 63 and expected_zone_idx != 63:
+                    expected_zone_idx = 0
+                else:
+                    expected_zone_idx += 1
+            
+            # After collecting a complete frame, add it to concat_frame_data
+            if second_cycle and reading_counter == 2:
+                i += 1
+                for zone_idx, zone_data in frame_data.items():
+                    if concat_frame_data[zone_idx] is None:
+                        concat_frame_data[zone_idx] = []
+                        concat_ambient_light[zone_idx] = []
+                    concat_frame_data[zone_idx].extend(zone_data)
+                    concat_ambient_light[zone_idx].append(ambient_light) # 1 per zone per reading (nx3 total)
+        return concat_frame_data, concat_ambient_light, distance_mm, range_sigma_mm
 
     def measurement_to_ros_msg(self, m, mcu_port, sensor_model):
         """
@@ -64,26 +88,25 @@ class VL53L8CHReader:
         Returns:
             ToFFrame: A ROS message containing the processed measurement data.
         """
+        concat_frame_data, concat_ambient_light, distance_mm, range_sigma_mm = m
         message = ToFFrame()
+        message.ambient_light = concat_ambient_light  # ambient light for each zone
         message.i2c_address = -1  # VL53L8CH does not use I2C address
         message.tick = -1 # VL53L8CH does not provide tick information
         message.num_valid_results = -1 # does not apply to VL53L8CH
         message.temperature = -1 # VL53L8CH does not provide temperature information
         message.measurement_num = -1 # VL53L8CH does not provide measurement number
         message.depth_estimates = [
-            DepthEstimate(depth_estimates=[], confidences=[]),
-            DepthEstimate(depth_estimates=[], confidences=[]),
+            DepthEstimate(depth_estimates=distance_mm, confidences=range_sigma_mm),
         ]
         message.serial_port = mcu_port
         message.sensor_model = sensor_model
-        message.histograms = [ToFHistogram(histogram=hist) for hist in m]
+        message.histograms = [ToFHistogram(histogram=hist) for hist in concat_frame_data]
         message.reference_histogram = ToFHistogram(histogram=[]) # VL53L8CH does not provide a reference histogram
 
         return message
 
-
-    @classmethod
-    def readline_and_decode(cls, ser):
+    def readline_and_decode(self, ser):
         """
         Given a serial port object denoting a port connected to an MCU with a VL3L8CH connected,
         read a line of data from the port and decode it
@@ -98,14 +121,43 @@ class VL53L8CHReader:
         """
 
         eol = b"\xFF\xFF\xFF\xFF"
+        distance_mm = None
+        range_sigma_mm = None
+        reading_counter = None
+        start_bin = None
 
-        c = ser.read_until(expected=eol)
+        c = self.ser.read_until(expected=eol)
         byte_listing = bytearray(c)
         length = len(byte_listing) - len(byte_listing) % 4
         line_as_floats = [
             struct.unpack("<f", bytes(byte_listing[i : i + 4]))[0] for i in range(0, length, 4)
         ]
 
-        # line_as_floats[-1] is always NaN, so remove it
-        # TODO figure out why this is and if we're missing out on data by doing this
-        return int(line_as_floats[0]), line_as_floats[1:-1], length
+        # extract data, see MCU code for ordering, line_as_floats[-1] is always thrown away
+        try:
+            zone_index = int(line_as_floats[0])  # zone index
+            ambient_light = line_as_floats[1]  # ambient light
+            hist = line_as_floats[2:-1] # histogram data
+        
+        except Exception as e:
+            print(f"Error parsing data: {e}")
+            raise ValueError(f"Failed to decode serial data: {e}")
+
+        # grab ending metadata
+        if zone_index == self.num_zones - 1:
+            c = self.ser.read_until(expected=eol)
+            byte_listing = bytearray(c)
+            length2 = len(byte_listing) - len(byte_listing) % 4
+            line_as_floats = [
+                struct.unpack("<f", bytes(byte_listing[i : i + 4]))[0] for i in range(0, length2, 4)
+            ]
+            try:
+                distance_mm = line_as_floats[:self.num_zones]
+                range_sigma_mm = line_as_floats[self.num_zones:-3]
+                reading_counter = int(line_as_floats[-3])  # readings_since_swap
+                start_bin = int(line_as_floats[-2])        # startBin value
+            except Exception as e:
+                print(f"Error reading metadata: {e}")
+                raise ValueError(f"Failed to decode metadata from serial: {e}")
+        
+        return zone_index, ambient_light, hist, length, distance_mm, range_sigma_mm, reading_counter, start_bin
